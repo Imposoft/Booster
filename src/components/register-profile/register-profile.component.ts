bd1a70e32d1c51a6e987cd5b02114b8cc5f79a5c
apatagg <arturosdg@gmail.com>
2020-10-21 15:07:37 +0200

    Musician creation finished

--- src/components/register-profile/register-profile.component.ts
+++ src/components/register-profile/register-profile.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Musician} from '../../models/musician/musician.model';
+import {SocialNetwork} from '../../models/profile/profile.model';
+import {Genre} from '../../models/genre/genre.model';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-register-profile',
@@ -13,7 +19,14 @@ export class RegisterProfileComponent implements OnInit {
   thirdFormGroup: FormGroup;
   selected = 'option2';
 
-  constructor(private formBuilder: FormBuilder) { }
+  musicianProfiles;
+
+  successMessage = '';
+  private _success = new Subject<string>();
+
+  constructor(private formBuilder: FormBuilder, private afs: AngularFirestore) {
+    this.musicianProfiles = afs.collection<Musician>('musicianProfiles');
+  }
 
   ngOnInit(): void {
     this.firstFormGroup = this.formBuilder.group({
@@ -30,5 +43,34 @@ export class RegisterProfileComponent implements OnInit {
       description: ['', [Validators.required]],
       precioSuscripcion: ['', [Validators.required]],
     });
+
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = '');
+
+  }
+
+  sendForm(): void {
+    if (this.firstFormGroup.value.profileRole  === 'musician'){
+      const musician = {
+        name: this.secondFormGroup.value.name,
+        email: this.secondFormGroup.value.email,
+        password: this.secondFormGroup.value.password,
+        imageSource: this.secondFormGroup.value.imageurl,
+        phone: 'ejemplotelef',
+        description: this.thirdFormGroup.value.description,
+        genres: ['EjemploGenero'],
+        location: this.thirdFormGroup.value.location,
+        socialNetworks: SocialNetwork.FACEBOOK,
+        subscriptionPrice: this.thirdFormGroup.value.precioSuscripcion
+      };
+      this.musicianProfiles.add(musician);
+      this._success.next('Perfil creado con exito!');
+    } else if (this.firstFormGroup.value.profileRole  === 'fan'){
+      // TODO Implement fan logic
+    } else if (this.firstFormGroup.value.profileRole  === 'band'){
+      // TODO Implement band logic
+    }
   }
 }
