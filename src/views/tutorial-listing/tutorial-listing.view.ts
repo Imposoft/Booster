8d4b12f8d30c057ec0dfbab619f145c8666ec6d9
apatagg <arturosdg@gmail.com>
2020-10-27 15:47:47 +0100

    Confirm modal when deleting user from class

--- src/views/tutorial-listing/tutorial-listing.view.ts
+++ src/views/tutorial-listing/tutorial-listing.view.ts
@@ -10,6 +10,7 @@ export class TutorialListingView implements OnInit {
   classList: Tutorial[];
 
   singleClass: Tutorial;
+  secondClass: Tutorial;
   constructor() {
     this.singleClass = {
       body: 'Cuerpo de la clase',
@@ -23,7 +24,19 @@ export class TutorialListingView implements OnInit {
         {id: '30', imageurl: 'assets/fan/avatar-man.jpg', name: 'Pablo2', contact: '611222334'},
         {id: '40', imageurl: 'assets/fan/avatar-man.jpg', name: 'Pablo3', contact: '611222336'}]
     };
-    this.classList = [this.singleClass , this.singleClass];
+    this.secondClass = {
+      body: 'Cuerpo de la clase',
+      description: 'Descripcion de la clase',
+      exclusive: false,
+      imgUrl: 'assets/class/guitarclass.jpg',
+      price: 25,
+      promoted: false,
+      title: 'Test Titulo',
+      userWaitList: [{id: '20', imageurl: 'assets/fan/avatar-man.jpg', name: 'Pedro', contact: '611222333'},
+        {id: '30', imageurl: 'assets/fan/avatar-man.jpg', name: 'Juan', contact: '611222334'},
+        {id: '40', imageurl: 'assets/fan/avatar-man.jpg', name: 'Patricio', contact: '611222336'}]
+    };
+    this.classList = [this.singleClass , this.secondClass];
   }
 
   ngOnInit(): void {
