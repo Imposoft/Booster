788f9c932032aeb545e0655d2bd225b990a7116e
leo-av <almohanovidick.noemi@gmail.com>
2020-11-01 13:51:24 +0100

    Update fan-modification.view.ts
    fallan name y phone, pero el resto de campos van bien

--- src/views/fan-modification/fan-modification.view.ts
+++ src/views/fan-modification/fan-modification.view.ts
@@ -21,9 +21,14 @@ export class FanModificationView implements OnInit {
 
   private _success = new Subject<string>();
   successMessage = '';
+  private nameModification: any;
+  private phoneModification: any;
+  private emailModification: any;
+  private imageModification: any;
+  private locModification: any;
 
   constructor(private _location: Location, private formBuilder: FormBuilder, private firestore: AngularFirestore) {
-    this.fanProfiles = firestore.collection<Fan>('fanProfiles');
+    // this.fanProfiles = firestore.collection<Fan>('fanProfiles');
     this.printedProfile = firestore.doc<Fan>('fanProfiles/NKUHb5YBHaCDQmSpWUFh');
     this.profile = this.printedProfile.valueChanges();
   }
@@ -44,12 +49,37 @@ export class FanModificationView implements OnInit {
   }
 
   sendForm(): void {
+    if (this.modificationForm.value.name === ''){
+      this.profile.subscribe((doc: { name: string; }) => { this.nameModification = doc.name; });
+    } else {
+      this.nameModification = this.modificationForm.value.name;
+    }
+    if (this.modificationForm.value.phone === ''){
+      this.profile.subscribe((doc: { phone: string; }) => { this.phoneModification = doc.phone; });
+    } else {
+      this.phoneModification = this.modificationForm.value.phone;
+    }
+    if (this.modificationForm.value.email === ''){
+      this.profile.subscribe((doc: { email: string; }) => { this.emailModification = doc.email; });
+    } else {
+      this.phoneModification = this.modificationForm.value.phone;
+    }
+    if (this.modificationForm.value.imageurl === ''){
+      this.profile.subscribe((doc: { imageSource: string; }) => { this.imageModification = doc.imageSource; });
+    } else {
+      this.imageModification = this.modificationForm.value.imageurl;
+    }
+    if (this.modificationForm.value.location === ''){
+      this.profile.subscribe((doc: { location: string; }) => { this.locModification = doc.location; });
+    } else {
+      this.locModification = this.modificationForm.value.location;
+    }
     const fan = {
-      name: this.modificationForm.value.name,
-      phone: this.modificationForm.value.phone,
-      email: this.modificationForm.value.email,
-      imageSource: this.modificationForm.value.imageurl,
-      location: this.modificationForm.value.location,
+      name: this.nameModification,
+      phone: this.phoneModification,
+      email: this.emailModification,
+      imageSource: this.imageModification,
+      location: this.locModification,
     };
     /*console.warn(fan.name + ';' +  + ';' + this.formBuilder.group(name).value.name);*/
     /*if (fan.name === '') {
