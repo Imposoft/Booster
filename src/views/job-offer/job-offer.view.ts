5aa0f546686d17c7e86d13ffd3ab291d1274427b
Daniel Núñez <danuena@inf.upv.es>
2020-11-16 12:02:49 +0100

    Cargar por id en el url de la web una oferta de trabajo (y definir una por defecto) - Working

--- src/views/job-offer/job-offer.view.ts
+++ src/views/job-offer/job-offer.view.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Tutorial} from '../../models/tutorial/tutorial.model';
+import {JobOffer} from '../../models/jobs/job-offer.model';
+import {Musician} from '../../models/musician/musician.model';
+import {Band} from '../../models/band/band.model';
+import {Genre} from '../../models/genre/genre.model';
 
 @Component({
   selector: 'app-job-offer',
@@ -8,8 +15,20 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class JobOfferView implements OnInit {
   public pathId: string;
+  private printedJobOffer: any;
+  public jobOffer: JobOffer;
+  public jobOfferOwner: Band;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  public userPathId: string;
+  private loggedId: string;
+  private printedProfile: any;
+
+  constructor(private router: Router, private route: ActivatedRoute, private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    // Oferta de trabajo  vacia sobre el que cargar los datos
+
+    // this.jobOffer = {body: '', exclusive: false, imgUrl: '', owner: undefined, budget: 0, promoted: false, title: '', genres: [], musician: [], band: [], endData: '', extraFiles: ''};
+    // this.jobOfferOwner = {description: '', email: '', members: Musician[], genres: [], imageSource: '', instruments: [], jobOffers: [], location: '', name: '', password: '', phone: '', socialNetworks: [], subscription: undefined, subscriptionPrice: 0, tutorials: []};
+
     // Recibimos el id del url de la web o en su defecto utilizamos uno por defecto
     this.route.params.subscribe( params => {
         if (params.id) {
@@ -17,8 +36,22 @@ export class JobOfferView implements OnInit {
         } else {
           this.pathId = 'NKUHb5YBHaCDQmSpWUFh';
         }
+
+      // this.printedJobOffer = afs.
+
+     /* this.printedJobOffer.valueChanges().subscribe(() => {
+        this.jobOffer = ;
+      }); */
       }
     );
+
+    // Cargamos el usuario owner de la oferta de trabajo
+    this.printedProfile = afs.doc<Band>('bandProfile/n6ZhZ1TJI7iayJS4GQrc');
+    this.userPathId = 'n6ZhZ1TJI7iayJS4GQrc';
+    this.printedProfile.valueChanges().subscribe((band) => {
+      this.jobOfferOwner = band;
+    });
+
   }
 
   ngOnInit(): void {
