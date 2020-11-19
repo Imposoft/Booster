3ba393f9ae12f2354b7f8e2aad4eb19d207ffe48
MrFla <73142409+MrFla@users.noreply.github.com>
2020-11-20 00:00:43 +0100

    card
     card

--- src/components/post-feed/post-feed.component.ts
+++ src/components/post-feed/post-feed.component.ts
@@ -13,18 +13,18 @@ export class PostFeedComponent implements OnInit {
 
   constructor() {
     this.postList = [];
-    this.postOne = {body: '',
+    this.postOne = {body: 'Me llamo Juana es una música estupenda y la verdad es que aconsejo a todos contratarla....cuerpo del feed1',
       exclusive: false,
-      imgUrl: '',
+      imgUrl: 'Aquí iría la imagen del feedd1?',
       owner: undefined,
       promoted: false,
-      title: ''};
-    this.postTwo = {body: '',
+      title: 'Titulo de Publicación 1'};
+    this.postTwo = {body: ' Me llamo Juana es una música muy poco profesional y la verdad que no aconsejo contratarta..cuerpo del feed 2......',
       exclusive: false,
-      imgUrl: '',
+      imgUrl: 'Aquí iría la imagen del feed2',
       owner: undefined,
       promoted: false,
-      title: ''};
+      title: 'Titulo de Publicación 2'};
     this.postList.push(this.postOne, this.postTwo);
   }
 
