f0c8fdd80117d9374a41288db36d362ada31747f
apatagg <arturosdg@gmail.com>
2020-11-20 09:50:32 +0100

    Visual fixes to post card

--- src/components/post-feed/post-feed.component.ts
+++ src/components/post-feed/post-feed.component.ts
@@ -15,13 +15,13 @@ export class PostFeedComponent implements OnInit {
     this.postList = [];
     this.postOne = {body: 'Me llamo Juana es una música estupenda y la verdad es que aconsejo a todos contratarla....cuerpo del feed1',
       exclusive: false,
-      imgUrl: 'Aquí iría la imagen del feedd1?',
+      imgUrl: 'https://firebasestorage.googleapis.com/v0/b/booster-dceeb.appspot.com/o/temp%2Fviolinista--644x362.jpg?alt=media&token=dcaeb0db-6f5e-4472-875b-3dd77de70c08',
       owner: undefined,
       promoted: true,
       title: 'Titulo de Publicación 1'};
     this.postTwo = {body: ' Me llamo Juana es una música muy poco profesional y la verdad que no aconsejo contratarta..cuerpo del feed 2......',
       exclusive: false,
-      imgUrl: 'Aquí iría la imagen del feed2',
+      imgUrl: 'https://firebasestorage.googleapis.com/v0/b/booster-dceeb.appspot.com/o/temp%2Fbisbal.png?alt=media&token=d34d3ee8-1f87-4ee0-8a94-ca939e67310a',
       owner: undefined,
       promoted: false,
       title: 'Titulo de Publicación 2'};
