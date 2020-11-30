707e3b43e3361142cf690fa1343f18475d0053b6
apatagg <arturosdg@gmail.com>
2020-11-30 17:47:24 +0100

    Protractor updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,8 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Booster app is running!');
+    console.log(page.getTitleText());
+    expect(page.getTitleText()).toEqual('Booster');
   });
 
   afterEach(async () => {
