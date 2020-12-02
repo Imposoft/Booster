03150e8d62e8de7be078b304d364b10bf4dec1e8
apatagg <arturosdg@gmail.com>
2020-12-02 17:25:06 +0100

    Updated tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -33,6 +33,69 @@ describe('workspace-project App', () => {
     expect(element(by.className('card-title')).getText()).toEqual('Iniciar sesion:');
   });
 
+  it('should load home page', () => {
+    browser.get('http://localhost:4200/home');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/home'));
+      });
+    }, 3000);
+  });
+
+  it('should load profile page', () => {
+    browser.get('http://localhost:4200/profile');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/profile'));
+      });
+    }, 3000);
+  });
+
+  it('should load fanProfile page', () => {
+    browser.get('http://localhost:4200/fanProfile');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/fanProfile'));
+      });
+    }, 3000);
+  });
+
+  it('should load jobOffer page', () => {
+    browser.get('http://localhost:4200/jobOffer');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/jobOffer'));
+      });
+    }, 3000);
+  });
+
+  it('should load tutorial page', () => {
+    browser.get('http://localhost:4200/tutorial');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/tutorial'));
+      });
+    }, 3000);
+  });
+
+  it('should load bandProfile page', () => {
+    browser.get('http://localhost:4200/bandProfile');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/bandProfile'));
+      });
+    }, 3000);
+  });
+
+  it('should load register page', () => {
+    browser.get('http://localhost:4200/register');
+    browser.wait( function() {
+      return browser.getCurrentUrl().then(function(url) {
+        return (url === ('http://localhost:4200/register'));
+      });
+    }, 3000);
+  });
+
   it('should log', async () => {
     browser.get('http://localhost:4200/');
     element(by.id('loginButton')).click();
