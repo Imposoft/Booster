cb0d858bcc3732beff07dd34f921f7f7c6ae43c8
NoobProgrammerUser <guivalca@eio.upv.es>
2020-12-05 22:00:46 +0100

    Perfiles top acabado.

--- src/views/home/home.view.ts
+++ src/views/home/home.view.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import { CalendarMonthViewDay, CalendarView } from 'angular-calendar';
-import { CalendarEvent, CalendarWeekViewBeforeRenderEvent } from 'angular-calendar';
-import { WeekViewHour, WeekViewHourColumn } from 'calendar-utils';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {Musician} from '../../models/musician/musician.model';
 import {map} from 'rxjs/operators';
+import {Profile} from '../../models/profile/profile.model';
 
 
 @Component({
@@ -23,33 +21,44 @@ import {map} from 'rxjs/operators';
 })
 export class HomeView implements OnInit {
   private perfiles: AngularFirestoreCollection<Musician>;
-  private musico: any;
-  private size: any;
-  private perfil: any;
+  private usersProfile: Profile[];
+  private usersAux: Profile[];
+  DropdownVar: number;
+  show: boolean;
 
-
-  constructor(private firestore: AngularFirestore) {
-    this.perfiles = firestore.collection<Musician>('musicianProfiles/', ref => ref.limitToLast(5).orderBy('name'));
-    this.perfiles = firestore.collection<Musician>('musicianProfiles/');
-    this.perfil = this.perfiles.snapshotChanges().pipe(
-      map(actions => {
-        return actions.map(a => {
-          const data = a.payload.doc.data() as Musician;
-          const id = a.payload.doc.id;
-          console.log(id);
-          // return { id, ...data };
-        });
-      })
-    );
-    this.musico = this.perfiles.snapshotChanges().subscribe(actions => {
-      return actions.map(a => {
-        const val = a.payload.doc.data();
-        // console.log(val.name);
-      });
-    });
-  }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.usersProfile = [];
+    this.usersAux = [];
+    this.perfiles = this.firestore.collection<Musician>('musicianProfiles/', ref => ref.limitToLast(10).orderBy('name'));
+    this.perfiles.snapshotChanges().subscribe(actions => {
+      return actions.map(a => {
+        const val = a.payload.doc.data();
+        this.usersProfile.push(val);
+      });
+    });
+    console.log('Entra');
+    this.usersProfile = this.mezclar(this.usersProfile);
+    console.log('Hace');
+  }
+
+  private mezclar(array: Profile[]): Profile[] {
+    console.log(array.length);
+    let currentIndex: any = array.length;
+    let temporaryValue: any;
+    let randomIndex: any;
+    console.log(currentIndex);
+    while (0 !== currentIndex) {
+      console.log('Jelou');
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+      console.log(randomIndex);
+    }
+    return array;
   }
 
 }
