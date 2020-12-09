d1fcdd5efb13262b1d08b68a7b6011c0f86e3dbd
apatagg <arturosdg@gmail.com>
2020-12-09 22:50:47 +0100

    JobOffer filter fix

--- src/components/search-filter/search-filter.component.ts
+++ src/components/search-filter/search-filter.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, PipeTransform} from '@angular/core';
-import {map, startWith} from 'rxjs/operators';
-import {DecimalPipe} from '@angular/common';
+import {Component, OnInit, PipeTransform, ChangeDetectorRef, ChangeDetectionStrategy} from '@angular/core';
+import {map, startWith, filter} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {FormControl} from '@angular/forms';
 import {JobOffer} from '../../models/jobOffer/job-offer.model';
@@ -10,8 +9,7 @@ import {Fan} from '../../models/fan/fan.model';
 @Component({
   selector: 'app-search-filter',
   templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.sass'],
-  providers: [DecimalPipe]
+  styleUrls: ['./search-filter.component.sass']
 })
 
 export class SearchFilterComponent implements OnInit {
@@ -21,11 +19,22 @@ export class SearchFilterComponent implements OnInit {
 
   jobOffers: Observable<JobOffer[]>;
 
-  constructor(pipe: DecimalPipe, private afs: AngularFirestore) {
-    this.jobOffers = this.afs.collection<JobOffer>('jobOfferPosts').valueChanges({idField: 'id'});
+  realJobOffers: JobOffer[];
 
-    this.filter.valueChanges.subscribe( text =>
-      this.jobOffers = this.afs.collection<JobOffer>('jobOfferPosts', ref => ref.orderBy('title').startAt(text).endAt(text + '\uf8ff')).valueChanges({idField: 'id'})
+  public trackItem(index: number, item: JobOffer): string {
+    return item.id;
+  }
+
+
+  constructor(private afs: AngularFirestore) {
+    this.jobOffers = this.afs.collection<JobOffer>('jobOfferPosts').valueChanges({idField: 'id'});
+    this.jobOffers.subscribe(value => this.realJobOffers = value);
+
+    this.filter.valueChanges.subscribe( text => {
+      this.jobOffers.pipe(
+        map(jobOffers => jobOffers.filter(jobOffer => jobOffer.title.toLowerCase().includes(text.toLowerCase())))
+      ).subscribe(value => this.realJobOffers = [...value]);
+    }
     );
   }
 
