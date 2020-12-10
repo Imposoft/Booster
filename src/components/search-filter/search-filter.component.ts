d683b5d836676c349f7379f901229816c5599487
leo-av <almohanovidick.noemi@gmail.com>
2020-12-10 14:08:05 +0100

    profile filters added
    musician band and fan separatedly

--- src/components/search-filter/search-filter.component.ts
+++ src/components/search-filter/search-filter.component.ts
@@ -5,6 +5,8 @@ import {FormControl} from '@angular/forms';
 import {JobOffer} from '../../models/jobOffer/job-offer.model';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Fan} from '../../models/fan/fan.model';
+import {Musician} from '../../models/musician/musician.model';
+import {Band} from '../../models/band/band.model';
 
 @Component({
   selector: 'app-search-filter',
@@ -18,8 +20,14 @@ export class SearchFilterComponent implements OnInit {
   model = 1;
 
   jobOffers: Observable<JobOffer[]>;
+  musicians: Observable<Musician[]>;
+  bands: Observable<Band[]>;
+  fans: Observable<Fan[]>;
 
   realJobOffers: JobOffer[];
+  realMusicians: Musician[];
+  realBands: Band[];
+  realFans: Fan[];
 
   public trackItem(index: number, item: JobOffer): string {
     return item.id;
@@ -35,6 +43,36 @@ export class SearchFilterComponent implements OnInit {
         map(jobOffers => jobOffers.filter(jobOffer => jobOffer.title.toLowerCase().includes(text.toLowerCase())))
       ).subscribe(value => this.realJobOffers = [...value]);
     }
+    );
+
+    this.musicians = this.afs.collection<Musician>('musicianProfiles').valueChanges({idField: 'id'});
+    this.musicians.subscribe(value => this.realMusicians = value);
+
+    this.filter.valueChanges.subscribe( text => {
+        this.musicians.pipe(
+          map(users => users.filter(user => user.name.toLowerCase().includes(text.toLowerCase())))
+        ).subscribe(value => this.realMusicians = [...value]);
+      }
+    );
+
+    this.bands = this.afs.collection<Band>('bandProfiles').valueChanges({idField: 'id'});
+    this.bands.subscribe(value => this.realBands = value);
+
+    this.filter.valueChanges.subscribe( text => {
+        this.bands.pipe(
+          map(users => users.filter(user => user.name.toLowerCase().includes(text.toLowerCase())))
+        ).subscribe(value => this.realBands = [...value]);
+      }
+    );
+
+    this.fans = this.afs.collection<Fan>('fanProfiles').valueChanges({idField: 'id'});
+    this.fans.subscribe(value => this.realFans = value);
+
+    this.filter.valueChanges.subscribe( text => {
+        this.fans.pipe(
+          map(users => users.filter(user => user.name.toLowerCase().includes(text.toLowerCase())))
+        ).subscribe(value => this.realFans = [...value]);
+      }
     );
   }
 
