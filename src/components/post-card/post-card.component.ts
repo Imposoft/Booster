4193f0c81529122becacf7436b3d76a5ecfa1f26
NoobProgrammerUser <guivalca@eio.upv.es>
2020-12-12 16:28:09 +0100

    Check user defined

--- src/components/post-card/post-card.component.ts
+++ src/components/post-card/post-card.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Post} from '../../models/post/post.model';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Router} from '@angular/router';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-post-card',
@@ -13,9 +15,17 @@ export class PostCardComponent implements OnInit {
   @Input() isOwner: boolean;
   commentsShown: boolean;
   public firestore: AngularFirestore;
+  public loggedId: string;
 
-  constructor(firestore: AngularFirestore, private router: Router) {
+  constructor(firestore: AngularFirestore, private router: Router, public afAuth: AngularFireAuth) {
     this.firestore = firestore;
+    this.loggedId = '';
+    // Si hemos iniciado sesion, loggedId sera nuestro id
+    this.afAuth.authState.subscribe(user => {
+      if (user){
+        this.loggedId = user.uid;
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -30,10 +40,11 @@ export class PostCardComponent implements OnInit {
   }
 
   toggleVisible(): void{
+    console.log(this.loggedId);
     this.commentsShown = !this.commentsShown;
   }
 
-  /*userLoggedIsProfileOwner(): boolean {
-    return this.loggedId === this.pathId;
-  }*/
+  userLoggedIsProfileOwner(): boolean {
+    return this.loggedId !== '';
+  }
 }
